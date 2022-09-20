aa9af5e2c2a775042e4b29f5ff93af9634aa45d6
AndrielFR <andrielkogama2@gmail.com>
2022-09-20 16:52:59 -0300

    handler: export missing types

--- src/handlers.rs
+++ src/handlers.rs
@@ -4,3 +4,5 @@
 pub use super::HandlerType::CallbackQuery as callback_query;
 pub use super::HandlerType::InlineQuery as inline_query;
 pub use super::HandlerType::Message as message;
+pub use super::HandlerType::MessageDeleted as message_deleted;
+pub use super::HandlerType::Raw as raw;
